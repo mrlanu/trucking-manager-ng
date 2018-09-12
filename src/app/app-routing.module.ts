d14dcd6322a04dc9892463ffdf4400839208cd4f
Serhiy K <mrlanu@gmail.com>
2018-09-11 22:09:37 -0500

    loadList page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
 import {SignupComponent} from './auth/signup/signup.component';
 import {LoadEditComponent} from './load/load-edit/load-edit.component';
+import {LoadListComponent} from './load/load-list/load-list.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'signUp', component: SignupComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'editLoad', component: LoadEditComponent}
+  {path: 'editLoad', component: LoadEditComponent},
+  {path: 'listLoad', component: LoadListComponent}
 ];
 
 @NgModule({
--- src/app/load/load-list/load-list.component.ts
+++ src/app/load/load-list/load-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {LoadModel} from '../load.model';
+import {LoadService} from '../load.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-load-list',
   templateUrl: './load-list.component.html',
   styleUrls: ['./load-list.component.css']
 })
-export class LoadListComponent implements OnInit {
+export class LoadListComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  private loadChangesSubs: Subscription;
+
+  displayedColumns = ['brokerName', 'weight', 'pickUpDate', 'deliveryDate'];
+  dataSource = new MatTableDataSource<LoadModel>();
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private loadService: LoadService) { }
 
   ngOnInit() {
+    this.loadChangesSubs = this.loadService.loadsChanged.subscribe((allLoads: LoadModel[]) => {
+      this.dataSource.data = allLoads;
+    });
+    this.loadService.fetchAvailableLoads();
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnDestroy() {
+    if (this.loadChangesSubs) {
+      this.loadChangesSubs.unsubscribe();
+    }
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
