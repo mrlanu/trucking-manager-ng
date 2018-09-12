21dd17cbc89031968fec450c9f8a25e88b97fe31
Serhiy K <mrlanu@gmail.com>
2018-09-11 21:18:54 -0500

    create load form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
 import {SignupComponent} from './auth/signup/signup.component';
+import {LoadEditComponent} from './load/load-edit/load-edit.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'signup', component: SignupComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'signUp', component: SignupComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'editLoad', component: LoadEditComponent}
 ];
 
 @NgModule({
--- src/app/load/load-edit/load-edit.component.ts
+++ src/app/load/load-edit/load-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {LoadService} from '../load.service';
 
 @Component({
   selector: 'app-load-edit',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoadEditComponent implements OnInit {
 
-  constructor() { }
+  loadForm: FormGroup;
+
+  constructor(private loadService: LoadService) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm() {
+    const brokerName = '';
+    const weight = '';
+    const pickUpDate = '';
+    const deliveryDate = '';
+
+    this.loadForm = new FormGroup({
+      'brokerName': new FormControl(brokerName, Validators.required),
+      'weight': new FormControl(weight),
+      'pickUpDate': new FormControl(pickUpDate, Validators.required),
+      'deliveryDate': new FormControl(deliveryDate, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    this.loadService.createLoad(this.loadForm.value);
   }
 
 }
