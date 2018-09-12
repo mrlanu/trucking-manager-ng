64c88cb5fdbfa928554b62adecec22fc2ac9f6d7
Serhiy K <mrlanu@gmail.com>
2018-09-11 20:01:09 -0500

    fetching all loads & create a load in loadService

--- src/app/load/load.service.ts
+++ src/app/load/load.service.ts
@@ -1,14 +1,39 @@
 import {LoadModel} from './load.model';
+import {Injectable} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {map} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
+@Injectable()
 export class LoadService {
 
   private loadList: LoadModel[] = [];
+  loadsChanged = new Subject<LoadModel[]>();
+
+  constructor(private db: AngularFirestore) {}
 
   createLoad(load: LoadModel) {
-    this.loadList.push(load);
+    this.db.collection('loads').add(load).then(result => {
+      const id = result.id;
+      this.db.doc(`loads/${id}`).update({id: id});
+      });
   }
 
-  getLoadList(): LoadModel[] {
-    return this.loadList.slice();
+  fetchAvailableLoads() {
+    this.db
+      .collection('loads')
+      .snapshotChanges()
+      .pipe(map(docsArray => {
+        return docsArray.map(doc => {
+          return {
+            ...doc.payload.doc.data() as LoadModel
+          };
+        });
+      })).subscribe((loads: LoadModel[]) => {
+        this.loadList = loads;
+        this.loadsChanged.next([...this.loadList]);
+        }
+      );
   }
+
 }
